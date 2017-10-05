cb3d60c49de378b8e4f1bfdee405d09067dfa632
ccosmesanmartin <ccosmesanmartin@victoria23.danielcastelao.org>
2017-10-05 09:18:58 +0200

    All

--- src/hola/mundo/HolaMundo.java
+++ src/hola/mundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println ("¡Hello!"); // TODO code application logic here
+       System.out.println ("¡Hello!"); // All code application logic here
        
     }
     
